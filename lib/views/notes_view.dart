1fd7180356e51857e368a068bc2f78d28b03f499
eljohnnyy <you@example.com>
2023-08-18 17:02:37 +0300

    show_button_sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../widget/notes_view_body.dart';
+import '../widget/show_button_sheet.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
@@ -8,7 +9,14 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-floatingActionButton: FloatingActionButton(onPressed: (){},child:const Icon(Icons.add,size: 31,),),
+floatingActionButton: FloatingActionButton(onPressed: (){
+showBottomSheet(context: context, builder: (context){
+return const AddNoteShowButton();
+});
+
+
+
+},child:const Icon(Icons.add,size: 31,),),
       body:const NotesViewBody(),
     );
   }
---
+++ lib/widget/show_button_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AddNoteShowButton extends StatelessWidget {
+  const AddNoteShowButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
