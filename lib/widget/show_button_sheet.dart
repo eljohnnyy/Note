11e05b7ab0e2746405e3104d59eddbb18b017a32
eljohnnyy <you@example.com>
2023-08-20 22:42:17 +0300

    add add cubit to button sheet

--- lib/widget/show_button_sheet.dart
+++ lib/widget/show_button_sheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note/cubits/cubit/add_note_cubit.dart';
 
 import 'package:note/widget/customtextfield.dart';
 
@@ -10,10 +13,19 @@ class AddNoteShowButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 16),
-      child:  SingleChildScrollView(
-        child: AddNoteForm(),
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+        
+          },
+          builder: (context, state) {
+            return  ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading?true:false,
+              child:const AddNoteForm());
+          },
+        ),
       ),
     );
   }
@@ -29,36 +41,46 @@ class AddNoteForm extends StatefulWidget {
 }
 
 class _AddNoteFormState extends State<AddNoteForm> {
- 
-  final GlobalKey<FormState>formkey=GlobalKey();
-  AutovalidateMode autovalidatemode=AutovalidateMode.disabled;
-  String? title,subtitle;
- @override
+  final GlobalKey<FormState> formkey = GlobalKey();
+  AutovalidateMode autovalidatemode = AutovalidateMode.disabled;
+  String? title, subtitle;
+  @override
   Widget build(BuildContext context) {
-    
     return Form(
       key: formkey,
       autovalidateMode: autovalidatemode,
-      child:  Column(children: [
-      const  SizedBox(height: 32,),
-        CustomTextField(hint: 'Title',onsaved: (p0) {
-          title=p0;
-        },),
-      const   SizedBox(height:16),
-      CustomTextField(hint: 'Content',max: 5,onsaved: (p0) {
-        subtitle=p0;
-      },),
-      const  SizedBox(height:32),
-       CustomButton(ontab: () {
-       if(formkey.currentState!.validate()){
-formkey.currentState!.save();
-       }
-       else{
-        autovalidatemode=AutovalidateMode.always;
-       }
-     },),
-     const  SizedBox(height:16),
-      ],),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            hint: 'Title',
+            onsaved: (p0) {
+              title = p0;
+            },
+          ),
+          const SizedBox(height: 16),
+          CustomTextField(
+            hint: 'Content',
+            max: 5,
+            onsaved: (p0) {
+              subtitle = p0;
+            },
+          ),
+          const SizedBox(height: 32),
+          CustomButton(
+            ontab: () {
+              if (formkey.currentState!.validate()) {
+                formkey.currentState!.save();
+              } else {
+                autovalidatemode = AutovalidateMode.always;
+              }
+            },
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
     );
   }
 }
